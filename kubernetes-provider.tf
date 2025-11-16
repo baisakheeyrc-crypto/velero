a85010ce31f0639367aeeabf0f7f7ff630fd5cd4
baishu_git <baisakhee.yrc@gmail.com>
2025-11-16 13:56:42 +0530

    Update kubernetes-provider.tf

--- kubernetes-provider.tf
+++ kubernetes-provider.tf
@@ -11,3 +11,15 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
+ # Add this for debugging
+ exec {
+ api_version = "client.authentication.k8s.io/v1beta1"
+ command = "aws"
+ args = [
+ "eks",
+ "get-token",
+ "--cluster-name",
+ "baishakhee-eks-new"
+ ]
+ }
+}
